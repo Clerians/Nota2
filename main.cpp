cb62287b60941d7fecc55aa2251121be5d2ef6dd
CL3P3 <165433361+Clerians@users.noreply.github.com>
2025-01-21 20:49:11 +0000

    a

--- main.cpp
+++ main.cpp
@@ -8,10 +8,44 @@ using namespace std;
 int main()
 {
     string nombre;
-    cout <<"nombre del archivo: " << endl;
+    cout <<"nombre del archivo(" "nombre.txt" "): " << endl;
     cin >> nombre;
 
     cout <<nombre << endl;
+
+    ifstream leerArch;
+    leerArch.open();
+    if(!leerArch.is_open()){
+        cerr << "No se pudo abrir el archivo." << endl;
+        return 1;
+    }
+
+    int n;
+    leerArch >> n;
+    leerArch.ignore();
+
+    int matriz[n][n];
+
+    string linea;
+
+    for(int i = 0 ; i < n ; ++i){
+        for(int j = 0 ; j < n - 1 ; ++j){
+            leerArch >> matriz[i][j];
+            if (j < n - 1) { 
+                leerArch.ignore(1, ','); 
+            }
+        }
+    }
+
+    leerArch.close();
+
+    cout << "Contenido de la matriz:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
