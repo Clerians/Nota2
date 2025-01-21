4af32cddc86b15de6f9ec35ea9117b6700813a17
CL3P3 <165433361+Clerians@users.noreply.github.com>
2025-01-21 20:52:06 +0000

    a

--- main.cpp
+++ main.cpp
@@ -30,8 +30,8 @@ int main()
 
     for(int i = 0 ; i < n ; ++i){
         for(int j = 0 ; j < n - 1 ; ++j){
-            leerArch >> matriz[i][j];
             if (j < n - 1) { 
+                leerArch >> matriz[i][j];
                 leerArch.ignore(1, ','); 
             }
         }
