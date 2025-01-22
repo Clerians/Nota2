316e4aa96b5ecb48d05e77865ed9dc0c84096f5c
CL3P3 <165433361+Clerians@users.noreply.github.com>
2025-01-22 20:49:52 +0000

    ñ

--- main.cpp
+++ main.cpp
@@ -14,31 +14,28 @@ class Nodo{
     }
 };
 
-Nodo* armarArbol(int n, int matriz[100][100], vector<Nodo*>& lista, vector<bool>& visitado){
+Nodo* armarArbol(int n, vector<vector<int>>& matriz, vector<Nodo*>& lista, vector<bool>& visitado){
     Nodo* raiz = lista[0];
     visitado[0] = true;
 
-    for(int i = 0; i < n ; ++i){
-        if(matriz[0][i] > 0 %% !visitado[i]){
-            Nodo* hijo = armarArbol(n,matriz,lista);
-            raiz->vecinos.push_back({hijo,matriz[0][i]});
+    for (int i = 0; i < n; ++i) {
+        if (matriz[0][i] > 0 && !visitado[i]) {
+            Nodo* hijo = lista[i];
+            raiz->vecinos.push_back({hijo, matriz[0][i]});
+            visitado[i] = true;
+            armarArbol(n, matriz, lista, visitado);
         }
     }
-
-    visitado[0] = false;
 
     return raiz;
 }
 
-void imprimir(Nodo* nodo){
+void imprimir(Nodo* nodo, string letra){
     if(nodo == nullptr) return;
 
-    cout <<nodo->nombres.first <<": "<< endl;
-    for(int i = 0; i < nodo->vecinos.size(); ++i){
-        Nodo* vecino = nodo->vecinos[i].first; 
-        int peso = nodo->vecinos[i].second;
-        cout << "  -> " << vecino->nombres.first << " Peso: " << peso << endl;
-        imprimir(vecino);
+    cout << letra << nodo->nombres.first << " Peso: " << nodo->nombres.second << endl;
+    for (size_t i = 0; i < nodo->vecinos.size(); ++i) {
+        imprimir(nodo->vecinos[i].first, letra + " ");
     }
 }
 
@@ -66,7 +63,7 @@ int main()
     leerArch >> n;
     leerArch.ignore();
 
-    int matriz[n][n];
+    vector<vector<int>> matriz(n, vector<int>(n));
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -81,7 +78,7 @@ int main()
 
     leerArch.close();
 
-    vector<bool> visitado(n, false);
+    
     vector<Nodo*> lista;
 
     for(int i = 0; i < n; ++i){
@@ -89,10 +86,11 @@ int main()
         lista.push_back(new Nodo(nodo,0));
     } 
 
-    Nodo* arbol = armarArbol(n,matriz,lista);
+    vector<bool> visitado(n, false);
+    Nodo* arbol = armarArbol(n,matriz,lista,visitado);
 
      cout << "Árbol a partir del nodo A:" << endl;
-     imprimir(arbol);
+     imprimir(arbol," ");
 
     //string letra;
     //cout <<"Ingresa el nodo destino (letra, ej. A-" << char('A' + n - 1) << "): ";
