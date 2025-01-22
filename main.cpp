8a9da009bd12115507950e636fec370533bd2331
CL3P3 <165433361+Clerians@users.noreply.github.com>
2025-01-22 20:26:45 +0000

    :P

--- main.cpp
+++ main.cpp
@@ -14,9 +14,26 @@ class Nodo{
     }
 };
 
-void dijkstra(){
+Nodo* armarArbol(int n, int matriz[][],vector<Nodo*>& lista){
+    Nodo* raiz = lista[0];
 
+    for(int i = 0; i < n ; ++i){
+        if(matriz[0][i] > 0){
+            Nodo* hijo = armarArbol(n,matriz,lista);
+            raiz->vecinos.puch_back({hijo,matriz[0][i]});
+        }
+    }
+    return raiz;
 }
+
+void imprimir(Nodo* nodo, string letra){
+    if(nodo == nullptr) return;
+
+    cout <<nodo->nombres.first << " Peso: " << nodo->nombres.second << endl;
+    imprimir(vecinos.first, " ");
+}
+
+//void dijkstra(){}
 
 int main()
 {
@@ -55,13 +72,26 @@ int main()
 
     leerArch.close();
 
-    string grafo;
-    cout <<"Ingresa el nodo destino (letra, ej. F): " << endl;
-    cin >> grafo;
 
-    int inicio = 0;
+    vector<Nodo*> lista;
 
-    
+    for(int i = 0; i < n; ++i){
+        char nodo = 'A' + i;
+        lista.puch_back(new Nodo(nodo,0));
+    } 
+
+    Nodo* arbol = armarArbol(n,matriz,lista);
+
+     cout << "Árbol a partir del nodo A:" << endl;
+     imprimir(arbol);
+
+    //string letra;
+    //cout <<"Ingresa el nodo destino (letra, ej. A-" << char('A' + n - 1) << "): ";
+    //cin >> letra;
+
+    //int final = letras - 'A';
+
+
 
     return 0;
 }
