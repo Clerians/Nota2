acb40c9915654993c23036c691f5236b4ff35db1
CL3P3 <165433361+Clerians@users.noreply.github.com>
2025-01-22 22:35:38 +0000

    a

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <queue>
+#include <climits>
 
 using namespace std;
 
@@ -15,28 +17,56 @@ public:
     }
 };
 
-Nodo* armarArbol(int n, vector<vector<int>>& matriz, vector<Nodo*>& lista, vector<bool>& visitado) {
-    Nodo* raiz = lista[0];
-    visitado[0] = true;
-
-    for (int i = 0; i < n; ++i) {
-        if (matriz[0][i] > 0 && !visitado[i]) {
-            Nodo* hijo = lista[i];
-            raiz->vecinos.push_back({hijo, matriz[0][i]});
-            visitado[i] = true;
-            armarArbol(n, matriz, lista, visitado);
+Nodo* armarArbol(int n, vector<vector<int>>& matriz, vector<Nodo*>& lista) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matriz[i][j] > 0) {
+                Nodo* hijo = lista[j];
+                int peso = matriz[i][j];
+                lista[i]->vecinos.push_back({hijo, peso});
+            }
         }
     }
-    return raiz;
+    return lista[0];
 }
 
-void imprimir(Nodo* nodo, string letra) {
-    if (nodo == nullptr) return;
+vector<char> dijkstra(Nodo* inicio, Nodo* fin, int n) {
+    vector<int> distancias(n, INT_MAX);
+    vector<Nodo*> previos(n, nullptr);
+    distancias[inicio->nombres.first - 'A'] = 0;
+    
+    queue<Nodo*> q;
+    q.push(inicio);
 
-    cout << letra << nodo->nombres.first << " Peso: " << nodo->nombres.second << endl;
-    for (size_t i = 0; i < nodo->vecinos.size(); ++i) {
-        imprimir(nodo->vecinos[i].first, letra + " ");
+    while (!q.empty()) {
+        Nodo* nodoActual = q.front();
+        q.pop();
+
+        for (int i = 0; i < nodoActual->vecinos.size(); i++) {
+            Nodo* nodoVecino = nodoActual->vecinos[i].first;
+            int peso = nodoActual->vecinos[i].second;
+            int indiceVecino = nodoVecino->nombres.first - 'A';
+
+            if (distancias[nodoActual->nombres.first - 'A'] + peso < distancias[indiceVecino]) {
+                distancias[indiceVecino] = distancias[nodoActual->nombres.first - 'A'] + peso;
+                previos[indiceVecino] = nodoActual;
+                q.push(nodoVecino);
+            }
+        }
     }
+
+    vector<char> camino;
+    for (Nodo* at = fin; at != nullptr; at = previos[at->nombres.first - 'A']) {
+        camino.push_back(at->nombres.first);
+    }
+
+    // Invertir el camino sin usar reverse()
+    vector<char> caminoInvertido;
+    for (int i = camino.size() - 1; i >= 0; i--) {
+        caminoInvertido.push_back(camino[i]);
+    }
+
+    return caminoInvertido;
 }
 
 int main() {
@@ -62,9 +92,8 @@ int main() {
 
     vector<vector<int>> matriz(n, vector<int>(n));
 
-    // Leer la matriz desde el archivo
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (j < n - 1) {
                 leerArch >> matriz[i][j];
                 leerArch.ignore(1, ',');
@@ -77,16 +106,29 @@ int main() {
     leerArch.close();
 
     vector<Nodo*> lista;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         char nodo = 'A' + i;
         lista.push_back(new Nodo(nodo, 0));
     }
 
-    cout << "Nodos existentes:" << endl;
-    for(int i = 0; i < n; i++){
-        cout << lista->vecinos[i].first << " ";
-    }
-    cout << endl;
+    Nodo* arbol = armarArbol(n, matriz, lista);
 
+    char final;
+    cout << "¿A dónde vamos? (camino de 'A' a " << char('A' + n - 1) << "): " << endl;
+    cin >> final;
+
+    int indice = final - 'A';
+
+    if (indice >= n) {
+        cout << "Destino no válido" << endl;
+    } else {
+        vector<char> camino = dijkstra(lista[0], lista[indice], n);
+        cout << "Camino más corto de A a " << final << ": ";
+        for (char nodo : camino) {
+            cout << nodo << " ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
